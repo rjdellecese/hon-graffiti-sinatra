136a0a1aae4fd713c490f6f231a4fb5118905fec
RJ Dellecese <rjdellecese@gmail.com>
2016-02-16 01:45:15 +0000

    Set up quotes model and basic routes.

--- app.rb
+++ app.rb
@@ -1,7 +1,22 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 require './config/environments' #database configuration
+require './models/quote'
 
 get '/' do
-  'Hello World!'
+  erb :index
+end
+
+post '/submit' do
+  @quote = Quote.new(params[:quote])
+  if @quote.save
+    redirect '/quotes'
+  else
+    'You messed SOMETHING up.'
+  end
+end
+
+get '/models' do
+  @quotes = Quote.all
+  erb :quotes
 end
