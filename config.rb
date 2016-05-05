29b18aa55ffa7486736b857a060c8970c37d0949
RJ Dellecese <rjdellecese@gmail.com>
2016-05-05 02:18:08 +0000

    Revert "Fix foundation icons, place hon fonts in own .scss file."
    This reverts commit 97a7d1562b3e0243e93e75c4d21c8b1b86302d86.

--- config.rb
+++ config.rb
@@ -1,6 +1,5 @@
 # Require any additional compass plugins here.
 add_import_path "bower_components/foundation/scss"
-add_import_path "bower_components/foundation/foundation-icons"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
